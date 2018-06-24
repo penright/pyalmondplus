753c23456ea9c241f0cd33ee8892b52f881febd7
Paul Enright <paul@enrights.net>
2018-06-23 23:06:56 -0500

    Current save

--- pyalmondplus/api.py
+++ pyalmondplus/api.py
@@ -6,33 +6,60 @@ import json
 
 class PyAlmondPlus:
 
-    api_url = None
-
-    def __init__(self, api_url):
+    def __init__(self, api_url, loop=asyncio.get_event_loop(), event_callback=None):
         self.api_url = api_url
-        print("Start")
-        print("start" + self.api_url)
-        asyncio.get_event_loop().run_until_complete(PyAlmondPlus.device_list(self))
+        self.ws = None
+        self.loop = loop
+        self.event_callback = event_callback
 
-    async def device_list(self):
-        print("function "+self.api_url)
-        # url_connect = "ws://192.168.1.2:7681/root/" + password
-        async with websockets.connect(self.api_url) as ws:
-            name = str(
-                '{"MobileInternalIndex":"231234","CommandType":"DeviceList"}')
-            await ws.send(name)
-            # print(f"> {name}")
-            greeting = await ws.recv()
-            print()
-            print(f"< {greeting}")
-            print()
-            await ws.send(name)
-            greeting = await ws.recv()
-            print()
-            print(f"< {greeting}")
-            print()
-            jsontest = json.loads(greeting)
-            print(jsontest['Devices'])
+    async def connect(self):
+        print("connecting")
+        if self.ws is None:
+            print("opening socket")
+            self.ws = await websockets.connect(self.api_url)
+        print(self.ws)
+
+    async def disconnect(self):
+        pass
+
+    async def send(self, message):
+        pass
+
+    async def receive(self):
+        print("receive started")
+        if self.ws is None:
+            await self.connect()
+        recv_data = await self.ws.recv()
+        print(recv_data)
+        await self.receive()
+
+    async def start(self):
+        print("Started")
+        asyncio.gather(self.receive(), loop=self.loop)
+        asyncio.set_event_loop(self.loop)
+
+    def stop(self):
+        pass
+
+    # async def device_list(self):
+    #     print("function "+self.api_url)
+    #     # url_connect = "ws://192.168.1.2:7681/root/" + password
+    #     async with websockets.connect(self.api_url) as ws:
+    #         name = str(
+    #             '{"MobileInternalIndex":"231234","CommandType":"DeviceList"}')
+    #         await ws.send(name)
+    #         # print(f"> {name}")
+    #         greeting = await ws.recv()
+    #         print()
+    #         print(f"< {greeting}")
+    #         print()
+    #         await ws.send(name)
+    #         greeting = await ws.recv()
+    #         print()
+    #         print(f"< {greeting}")
+    #         print()
+    #         jsontest = json.loads(greeting)
+    #         print(jsontest['Devices'])
 
 
 #
--- pyalmondplus/cli.py
+++ pyalmondplus/cli.py
@@ -2,19 +2,36 @@
 
 """Console script for pyalmondplus."""
 import sys
+import asyncio
+import time
+import threading
 import click
 import pyalmondplus.api
 
-
+loop = None
 @click.command()
 @click.option('--url', default='')
+
 def main(url):
     """Console script for pyalmondplus."""
     click.echo("Connecting to " + url)
-    almond_devices = pyalmondplus.api.PyAlmondPlus(url)
-
+    almond_devices = pyalmondplus.api.PyAlmondPlus(url, loop)
+    almond_devices.start()
+    print("Connected to Almond+")
+    while True:
+        value = click.prompt("What next: ")
+        print("command is: " + value)
+        if value == "stop":
+            break
+    almond_devices.stop()
     return 0
 
+def LoopStart():
+    global loop
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
 
 if __name__ == "__main__":
+    t = threading.Thread(target=LoopStart())
+    t.start()
     sys.exit(main())  # pragma: no cover
